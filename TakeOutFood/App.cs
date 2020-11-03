ecb4dd1eee6eee60dd6c463ebc8c4249c0497e34
Ge Luxue <lge5@slb.com>
2020-11-03 20:13:48 +0800

    Commit c# test.

--- TakeOutFood/App.cs
+++ TakeOutFood/App.cs
@@ -16,9 +16,65 @@
 
         public string BestCharge(List<string> inputs)
         {
-            //TODO: write code here
+            List<Item> allItems = itemRepository.FindAll();
+            List<SalesPromotion> allPromotions=salesPromotionRepository.FindAll();
+            string purchaseMsg = "============= Order details =============\n";
+            string promotionMsg = "-----------------------------------\n" +
+                    "Promotion used:\n";
+            double savedMoney = 0;
+            double costMoney = 0;
+            double finalCost = 0;
+            string promotionItems = "";
+            string output="";
+            foreach (string a in inputs) 
+            {
+                var record = a.Split('x');
+                var id = record[0].Trim();
+                string name="";
+                double number = double.Parse(record[1]);
+                double price =0;
+                bool isOnSale = IsOnSale(id);              
+                foreach (Item b in allItems) 
+                {
+                    if (id == b.Id) 
+                    {
+                        name = b.Name;
+                        price = b.Price;
 
-            return null;
+                        if (isOnSale)
+                        {
+                            promotionItems += name;
+                            savedMoney += b.Price * number * 0.5;
+                        }
+                        else 
+                        {
+                            promotionMsg = "";
+                        }                      
+                    }
+                }
+                var recordPrice = price * number;
+                costMoney += recordPrice;
+                purchaseMsg += name + " x " + number + " = " + recordPrice + "\n";
+            }
+            finalCost = costMoney - savedMoney;
+            promotionMsg += promotionMsg + "(" + promotionItems + "), saving" + savedMoney + " yuan\n";
+            output = purchaseMsg + promotionMsg;
+            output += "Total: " + finalCost + "yuan\n"+ "===================================";           
+            return output;
+            Console.WriteLine(output);
+        }
+
+        public bool IsOnSale(string input)
+        {
+            List<SalesPromotion> allPromotions = salesPromotionRepository.FindAll();
+            foreach (SalesPromotion a in allPromotions)
+            {
+                if (input == a.Type)
+                {
+                    return true;
+                }              
+            }
+            return false;
         }
     }
 }
